798c706d67e8faa29a9c1212372fd7be06757493
권낙현 <knh6269@gmail.com>
2021-06-08 00:01:01 +0900

    favorite

--- Mandish/app/src/main/java/com/example/mandish_lilac/favorites.java
+++ Mandish/app/src/main/java/com/example/mandish_lilac/favorites.java
@@ -1,14 +1,61 @@
 package com.example.mandish_lilac;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class favorites extends AppCompatActivity {
+    ListView favoite_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid=user!=null?user.getUid():null;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        ListView listView=(ListView)findViewById(R.id.result_listView);
+        final ArrayAdapter adapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+        listView.setAdapter(adapter);
+
+        DatabaseReference database= FirebaseDatabase.getInstance().getReference();
+        database.child("UserAccount").child(uid).child("RecRecipe").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
