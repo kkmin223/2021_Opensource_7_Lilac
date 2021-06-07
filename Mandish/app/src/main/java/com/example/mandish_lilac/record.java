7b3da1ca31bc2fee090d2e74a6ea69a2bed09009
kkmin223 <rkdals133@gmail.com>
2021-06-07 23:24:03 +0900

    backbtn

--- Mandish/app/src/main/java/com/example/mandish_lilac/record.java
+++ Mandish/app/src/main/java/com/example/mandish_lilac/record.java
@@ -3,12 +3,22 @@ package com.example.mandish_lilac;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class record extends AppCompatActivity {
-
+    private ImageButton back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+        back = findViewById(R.id.backbtn5);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
